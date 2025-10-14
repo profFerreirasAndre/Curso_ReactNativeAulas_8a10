24cd9b412b621e1e1db1ad3c611dddfd933e5f2f
Andre Luiz De Souza Ferreira <andlferreira@firjan.com.br>
2025-10-13 21:22:16 -0300

    update

--- Aula8/App.js
+++ Aula8/App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Scroll1 from './src/components/Aula8Scroll';
 import Flat from './src/components/Aula8_FlatList';
 import Aula9Picker from './src/components/Aula9_Picker';
+import Aula8_1FlatList from './src/components/Aula 8.1_flatList';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <Aula9Picker/>
+     <Aula8_1FlatList/>
      </View>
   );
 }
